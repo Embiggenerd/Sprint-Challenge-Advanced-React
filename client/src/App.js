3b236a7df0295ade3c3d375824aafffbb8891882
igor <embiggenerd@gmail.com>
2020-01-17 13:05:55 -0500

    basic testing started

--- client/src/App.js
+++ client/src/App.js
@@ -21,7 +21,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="App" data-testid="App">
      <PlayerList playerList={this.state.players}/>
       </div>
     )
--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom'
 import App from './App';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+test('renders parent component', () => {
+  const { getByTestId } = render(<App />);
+  const parent = getByTestId(/app/i);
+  expect(parent).toBeInTheDocument();
 });
